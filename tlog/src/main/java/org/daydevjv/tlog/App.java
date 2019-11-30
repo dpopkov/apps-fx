ef2f9ab492b085201c2bb309de9957f0dbb82f0c
Denis Popkov <pkvdenis@gmail.com>
2019-11-30 17:03:21 +0300

    tlog: created dummy project for tlog

---
+++ tlog/src/main/java/org/daydevjv/tlog/App.java
@@ -0,0 +1,13 @@
+package org.daydevjv.tlog;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
