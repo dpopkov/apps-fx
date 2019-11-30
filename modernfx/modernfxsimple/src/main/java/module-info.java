1aed33c6b0367582394a301c26c09645e673996d
Denis Popkov <pkvdenis@gmail.com>
2019-11-30 17:38:59 +0300

    modernfx: add module modernfxsimple

---
+++ modernfx/modernfxsimple/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module org.daydevjv.modernfx.modernfxsimple {
+    requires javafx.controls;
+    exports org.daydevjv.modernfx.modernfxsimple;
+}
---
+++ modernfx/modernfxsimple/src/main/java/org/daydevjv/modernfx/modernfxsimple/SystemInfo.java
@@ -0,0 +1,13 @@
+package org.daydevjv.modernfx.modernfxsimple;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
